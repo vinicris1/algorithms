c1300eabc767c7c7c7e5d3556cfa1b907e36deef
vinicris1 <viniciuscris277@gmail.com>
2026-09-16 21:16:40 -0300

    start void double linked list

---
+++ linear_data_struct/void_double_linked_list.c
@@ -0,0 +1,3 @@
+// this double linked list have an function to accept data where the user request to put, like if the user want to put data in the 3 element of the list it will be possible
+// also remove elements it's possible by the same way
+// this is a void list so the user can use any type of data inside the list
